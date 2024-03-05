eb44613ac6b6a489abeed899791c2166193e0a44
AndrewKuliahin96 <kulagin.andrew38@gmail.com>
2024-03-05 21:58:03 +0200

    Updated publishing, added nexus plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import com.android.build.api.dsl.LibraryPublishing
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
+    `maven-publish`
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0-rc-2"
 }
 
 buildscript {
@@ -37,5 +41,36 @@ allprojects {
         classpath = kotlinLint
         args("-F", "src/**/*.kt")
         jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED")
+    }
+}
+
+subprojects {
+    if (plugins.hasPlugin("com.android.library")) {
+        configure<LibraryPublishing> {
+            singleVariant("release") {
+                withSourcesJar()
+            }
+        }
+    }
+}
+
+tasks.create<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
+
+ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
+ext["signing.key"] = System.getenv("SIGNING_KEY")
+
+nexusPublishing.repositories {
+    sonatype {
+        nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+        snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
+        stagingProfileId.set(System.getenv("SONATYPE_STAGING_PROFILE_ID"))
+
+        username.set(System.getenv("OSSRH_USERNAME"))
+        password.set(System.getenv("OSSRH_PASSWORD"))
+        version = System.getenv("RELEASE_VERSION")
     }
 }
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,9 +2,13 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id("kotlin-parcelize")
-    `maven-publish`
-    signing
 }
+
+extra.set("GROUP_ID", "com.kuliahin")
+extra.set("ARTIFACT_ID", "compose.calendar-view")
+extra.set("VERSION", System.getenv("RELEASE_VERSION"))
+
+apply(from = "${rootDir}/scripts/publish-module.gradle")
 
 android {
     defaultConfig {
@@ -68,73 +72,4 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-}
-
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "com.kuliahin"
-            artifactId = "calendar-view"
-            version = System.getenv("RELEASE_VERSION")
-
-            afterEvaluate {
-                from(components["release"])
-            }
-
-            pom {
-                pom.name = "Compose Calendar View"
-                pom.packaging = "aar"
-                pom.description = "Compose Calendar View (Android)"
-                pom.url = "https://github.com/AndrewKuliahin96/compose-calendar-view"
-
-                scm {
-                    url.set("https://github.com/AndrewKuliahin96/compose-calendar-view")
-                    connection = "scm:git@github.com:AndrewKuliahin96/compose-calendar-view.git"
-                    developerConnection = "scm:git@github.com:AndrewKuliahin96/compose-calendar-view.git"
-                }
-
-                licenses {
-                    license {
-                        name = "MIT license"
-                        url = "https://github.com/AndrewKuliahin96/compose-calendar-view/blob/main/LICENSE"
-                    }
-                }
-
-                developers {
-                    developer {
-                        id = "andrew_kuliahin"
-                        name = "Andrew Kuliahin"
-                        email = "kulagin.andrew38@gmail.com"
-                    }
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-
-            val isSnapshot = version.toString().endsWith("SNAPSHOT")
-
-            setUrl(provider {
-                if (isSnapshot) snapshotsRepoUrl else releasesRepoUrl
-            })
-
-            credentials {
-                username = System.getenv("OSSRH_USERNAME")
-                password = System.getenv("OSSRH_PASSWORD")
-            }
-        }
-    }
-}
-
-signing {
-    useInMemoryPgpKeys(
-        System.getenv("SIGNING_KEY_ID"),
-        System.getenv("SIGNING_KEY"),
-        System.getenv("SIGNING_PASSWORD")
-    )
-    sign(publishing.publications)
 }
